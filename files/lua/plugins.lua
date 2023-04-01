af8f068519c01d37fb0ae9d260f69ac266524390
Aleksandr B. Ivanov <radanisk@gmail.com>
2023-04-01 19:30:19 +0300

    Use release branch for telescope.nvim

--- files/lua/plugins.lua
+++ files/lua/plugins.lua
@@ -38,7 +38,7 @@ return require('packer').startup(function()
   }
 
   use {
-    'nvim-telescope/telescope.nvim',
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
