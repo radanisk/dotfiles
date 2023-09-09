ca15fc54229fcc442c46bbbade3df0816c811b19
Aleksandr B. Ivanov <radanisk@gmail.com>
2023-09-09 19:59:01 +0300

    Add search word

--- files/init.lua
+++ files/init.lua
@@ -85,7 +85,15 @@ require('lazy').setup({
   },
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      { "<leader>ff", function() require('telescope.builtin').find_files() end },
+      { "<leader>sg", function() require('telescope.builtin').live_grep() end },
+      { "<leader>sb", function() require('telescope.builtin').buffers() end },
+      { "<leader>sh", function() require('telescope.builtin').help_tags() end },
+      { "<leader>sw", function() require('telescope.builtin').grep_string({ word_match = '-w' }) end, desc = "Word" },
+      { "<leader>sw", function() require('telescope.builtin').grep_string() end, mode = "v", desc = "Selection" },
+    },
   },
   'jvirtanen/vim-hcl',
   'tpope/vim-bundler',
@@ -128,7 +136,10 @@ require('lazy').setup({
   { 'weirongxu/plantuml-previewer.vim'},
   {
     'nvim-pack/nvim-spectre',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+    }
   },
 })
 
@@ -174,18 +185,6 @@ bind("n", "<C-h>", "<C-w>h", opts)
 bind("n", "<C-j>", "<C-w>j", opts)
 bind("n", "<C-k>", "<C-w>k", opts)
 
--- Refactor with spectre
-bind('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-  desc = "Search current word"
-})
-bind('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-  desc = "Search current word"
-})
-
-bind("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-bind("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
-bind("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
-bind("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
 --
 bind('n', '<leader><leader>', ':NvimTreeToggle<CR>')
 
