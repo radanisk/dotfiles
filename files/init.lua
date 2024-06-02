da7bc22f79b85de6e00049c2a7d50c95f62e3c15
Aleksandr B. Ivanov <radanisk@gmail.com>
2024-06-02 14:00:56 +0300

    Remove Comment.nvim

--- files/init.lua
+++ files/init.lua
@@ -122,12 +122,6 @@ require('lazy').setup({
     end
   },
   {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  },
-  {
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup()
