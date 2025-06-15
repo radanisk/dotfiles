c5844babfbfab1706cc2fac73341fdddb621daec
Aleksandr B. Ivanov <radanisk@gmail.com>
2025-06-15 12:44:55 +0300

    Add codecompanion.nvim

--- files/init.lua
+++ files/init.lua
@@ -254,6 +254,39 @@ require('lazy').setup({
       scope = { show_start = false, show_end = false },
     },
     main = "ibl",
+  },
+  {
+  "olimorris/codecompanion.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+},
+})
+
+require("codecompanion").setup({
+  adapters = {
+    coder = function()
+      return require("codecompanion.adapters").extend("openai_compatible", {
+        schema = {
+          model = {
+            default = "coder",
+          },
+        },
+      })
+    end,
+  },
+  strategies = {
+    chat = {
+      adapter = "coder",
+    },
+    inline = {
+      adapter = "coder",
+    },
+    cmd = {
+      adapter = "coder",
+    }
   }
 })
 
@@ -290,3 +323,9 @@ bind("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 bind("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 bind("n", "<leader>bd", function() Snacks.bufdelete() end, { desc = "Delete Buffer" })
 bind("n", "<leader>bo", function() Snacks.bufdelete.other() end, { desc = "Delete Other Buffers" })
+
+vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
