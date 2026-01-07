2f28bbdfc53e99d9b442cd263dab1de30ca82709
Aleksandr B. Ivanov <radanisk@gmail.com>
2026-01-07 15:14:38 +0300

    Add Mason for LSP servers

--- files/lua/plugins/lsp.lua
+++ files/lua/plugins/lsp.lua
@@ -1,5 +1,17 @@
 return {
   {
+    'williamboman/mason.nvim',
+    opts = {},
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    opts = {
+      ensure_installed = { 'gopls', 'solargraph' },
+      automatic_installation = true,
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       vim.lsp.enable('gopls')
